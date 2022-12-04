15e377e30a8f5f5de9278cf5ee65d85134990524
wahid14k <wahid14k@gmail.com>
2022-12-05 02:53:02 +0600

    menubar is done with mobile version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-
+import Navbar from './components/nav'
+import Footer from './components/footer'
+import Head from './components/head'
 function App() {
   return (
-    <div className="App">
-      app
+    <>
+    <div className="App h-full w-full">
+      <Navbar/>
+      <Head/>
+      
     </div>
+    
+    </>
   );
 }
 
