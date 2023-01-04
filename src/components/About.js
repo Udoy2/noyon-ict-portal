3e115273781552ead44bfcb26c5837b0ef13a5c9
wahid14k <wahid14k@gmail.com>
2023-01-04 22:10:45 +0600

    footer done

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,10 @@ import Slide from './carousel'
 import AboutPng from './about.png'
 const About = () => {
   return (
-    <div className='bg-[#fff] w-full py-8'>
+    <div className='bg-[#fff] w-full pt-8'>
         <h1 className='text-center text-5xl font-bold py-4'>জনপ্রিয় কোর্সসমূহ</h1>
         <Slide/>
         <div style={ { backgroundImage: `url(${AboutPng})` } } className='h-[71px] sm:h-[100px] md:h-[118px] mt-[-10px] xl:h-[300px] lg:h-[260px] md:w-auto bg-cover md:mt-[-100px]'>
-            
             </div>
     </div>
   )
